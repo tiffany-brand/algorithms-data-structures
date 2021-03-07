030964d2da3e7d76e22c9eec014416f4b608c18e
Tiffany Brand <brand2learn@gmail.com>
2021-03-07 17:28:19 -0500

    Next challenge defined

---
+++ algorithms/averagePair.js
@@ -0,0 +1,7 @@
+// Given a sorted array of integers and a target average
+// determine if there is a pair of values where the
+// average equals the target average. More than one pair may match.
+
+const averagePair = (arr, targetAvg) => {
+
+}
