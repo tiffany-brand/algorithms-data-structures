8f7cd5c4c347c8996cc41494674b0152ee27656a
Tiffany Brand <brand2learn@gmail.com>
2021-03-09 22:31:58 -0500

    Refactored

--- algorithms/fibRecursive.js
+++ algorithms/fibRecursive.js
@@ -3,27 +3,13 @@
 // fib(28) // 317811
 // fib(35) // 9227465
 
-function fib(num) {
-	// current fib number
-	let result = 2;
-	// previous two numbers
-	let prev1 = 1;
-	let prev2 = 1;
-
-	// calculate fib numbers up to num
-	const helper = (current) => {
-		// end condition
-		if (current === num) return;
-		// fib number calculation
-		result = prev1 + prev2;
-		prev2 = prev1;
-		prev1 = result;
-
-		return helper(current + 1);
-	};
-
-	helper(2);
-	return result;
-}
+// refactored
+const fib = (num) => {
+	if (num <= 2) return 1;
+	return fib(num - 1) + fib(num - 2);
+};
 
 console.log(fib(35));
+console.log(fib(28));
+console.log(fib(10));
+console.log(fib(4));
