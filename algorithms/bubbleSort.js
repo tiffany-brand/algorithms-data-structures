2ecda7216100956a59ef3b2aa8c42f20a9d2e96b
Tiffany Brand <brand2learn@gmail.com>
2021-03-16 16:54:05 -0400

    Optimization

--- algorithms/bubbleSort.js
+++ algorithms/bubbleSort.js
@@ -12,21 +12,28 @@ const swap2 = (arr, idx1, idx2) => {
 }
 
 const bubbleSort = (arr) => {
+    // variable to mark if a swap occurred
+    let noSwaps;
     // start looping from the end of the array to the beginning using i
     for (let i = arr.length - 1; i >= 0; i--) {
+        noSwaps = true;
         // inner loop  j goes from the beginning until i-1
         // because the end will become sorted, so we don't have to 
         // loop through those sorted numbers
         for (let j = 0; j <= i - 1; j++) {
+            console.log(arr, arr[j], arr[j + 1]);
             // if arr[j] is greater than arr[j+1], swap the values 
-            if (arr[j] > arr[j + 1]) swap2(arr, j, j + 1)
+            if (arr[j] > arr[j + 1]) {
+                swap2(arr, j, j + 1)
+                noSwaps = false;
+            }
         }
-
+        if (noSwaps) break;
     }
     // return the sorted array
     return arr
 }
 
-let arr1 = [8, 4, 55, 2, 1, 9, 0];
+let arr1 = [8, 1, 2, 3, 4, 5];
 
 console.log(bubbleSort(arr1));
